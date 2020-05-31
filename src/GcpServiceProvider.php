552ca17cd8ad559f7bf434441ab2862b1e0e3a3f
Michał Prusak <michal.prusak@itsailors.tech>
2020-05-31 15:49:43 +0200

    Service Provider dla biblioteki

---
+++ src/GcpServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ItSailors\Gcp;
+
+use Illuminate\Routing\Route;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Dostawca usług dla tej biblioteki. Pozwala na wywołanie w docelowej aplikacji
+ * funkcjonalności tej biblioteki i rejestruje potrzebne zależności.
+ *
+ * @author Michał Prusak <michal.prusak@itsailors.tech>
+ * @package ItSailors\Gcp
+ */
+class GcpServiceProvider extends ServiceProvider
+{
+    /**
+     * Zebranie i rozruch usług dostarczanych przez tę bibliotekę.
+     * Tu idą wszystkie deklaracje ścieżek, operacje kopiowania lub
+     * scalania plików konfiguracyjnych, itd.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(base_path("routes.php"));
+    }
+}
